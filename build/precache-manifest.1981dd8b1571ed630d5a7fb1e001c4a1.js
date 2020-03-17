d92e3b7a9148d5699ca8b5d46a778ed4b7c9276f
Ksenia Kuntsevich <kse.kuntsevich@icloud.com>
2020-03-17 23:28:01 +0300

    feat: deploy build

---
+++ build/precache-manifest.1981dd8b1571ed630d5a7fb1e001c4a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35b6eff974bb05806f00937a735b922f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3622462aa7b18e8f3045",
+    "url": "./static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "64f350407011ea36691e",
+    "url": "./static/css/main.974bef4d.chunk.css"
+  },
+  {
+    "revision": "3622462aa7b18e8f3045",
+    "url": "./static/js/2.14f8cc7f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "./static/js/2.14f8cc7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64f350407011ea36691e",
+    "url": "./static/js/main.852be764.chunk.js"
+  },
+  {
+    "revision": "e6398e6ef302edff7b00",
+    "url": "./static/js/runtime-main.23a781e4.js"
+  }
+]);
